eb8ed16094f70f69fd520373185d4d4f729522e0
bestruirui <bestrui@bestrui.top>
2025-07-09 11:14:54 +0800

    feat: :sparkles: 系统配置的获取

--- internal/config/proxy.go
+++ internal/config/proxy.go
@@ -9,61 +9,40 @@ import (
 	"github.com/bestruirui/bestsub/internal/models/system"
 )
 
-func GetProxyConfig() (*system.Proxy, error) {
+func GetProxyConfig() (*system.ProxyConfig, error) {
 	ctx := context.Background()
 	configRepo := database.SystemConfig()
 
-	config := &system.Proxy{}
-
-	enableConfig, err := configRepo.GetByKey(ctx, "proxy.enable")
+	// 使用 GetConfigsByGroup 批量获取 proxy 组的所有配置
+	configs, err := configRepo.GetConfigsByGroup(ctx, "proxy")
 	if err != nil {
-		return nil, fmt.Errorf("failed to get proxy.enable: %w", err)
-	}
-	if enableConfig != nil {
-		config.Enable = enableConfig.Value == "true"
+		return nil, fmt.Errorf("failed to get proxy configs: %w", err)
 	}
 
-	typeConfig, err := configRepo.GetByKey(ctx, "proxy.type")
-	if err != nil {
-		return nil, fmt.Errorf("failed to get proxy.type: %w", err)
-	}
-	if typeConfig != nil {
-		config.Type = typeConfig.Value
-	}
+	config := &system.ProxyConfig{}
 
-	hostConfig, err := configRepo.GetByKey(ctx, "proxy.host")
-	if err != nil {
-		return nil, fmt.Errorf("failed to get proxy.host: %w", err)
-	}
-	if hostConfig != nil {
-		config.Host = hostConfig.Value
-	}
-	portConfig, err := configRepo.GetByKey(ctx, "proxy.port")
-	if err != nil {
-		return nil, fmt.Errorf("failed to get proxy.port: %w", err)
-	}
-	if portConfig != nil && portConfig.Value != "" {
-		port, err := strconv.Atoi(portConfig.Value)
-		if err != nil {
-			return nil, fmt.Errorf("invalid proxy port: %w", err)
+	// 遍历配置项，根据键设置对应的字段值
+	for _, cfg := range configs {
+		switch cfg.Key {
+		case "proxy.enable":
+			config.Enable = cfg.Value == "true"
+		case "proxy.type":
+			config.Type = cfg.Value
+		case "proxy.host":
+			config.Host = cfg.Value
+		case "proxy.port":
+			if cfg.Value != "" {
+				port, err := strconv.Atoi(cfg.Value)
+				if err != nil {
+					return nil, fmt.Errorf("invalid proxy port: %w", err)
+				}
+				config.Port = port
+			}
+		case "proxy.username":
+			config.Username = cfg.Value
+		case "proxy.password":
+			config.Password = cfg.Value
 		}
-		config.Port = port
-	}
-
-	usernameConfig, err := configRepo.GetByKey(ctx, "proxy.username")
-	if err != nil {
-		return nil, fmt.Errorf("failed to get proxy.username: %w", err)
-	}
-	if usernameConfig != nil {
-		config.Username = usernameConfig.Value
-	}
-
-	passwordConfig, err := configRepo.GetByKey(ctx, "proxy.password")
-	if err != nil {
-		return nil, fmt.Errorf("failed to get proxy.password: %w", err)
-	}
-	if passwordConfig != nil {
-		config.Password = passwordConfig.Value
 	}
 
 	return config, nil
