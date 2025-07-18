fc3af05d6c07945958aed282a48041a751a78aa2
bestruirui <bestrui@bestrui.top>
2025-07-18 14:10:10 +0800

    feat: :sparkles: 身份验证缓存

---
+++ internal/database/op/auth.go
@@ -0,0 +1,65 @@
+package op
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bestruirui/bestsub/internal/database/interfaces"
+	"github.com/bestruirui/bestsub/internal/models/auth"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var authRepo interfaces.AuthRepository
+var authData *auth.Data
+
+func AuthRepo() interfaces.AuthRepository {
+	if authRepo == nil {
+		authRepo = repo.Auth()
+	}
+	return authRepo
+}
+
+func AuthGet() (auth.Data, error) {
+	var err error
+	if authData == nil {
+		authData, err = AuthRepo().Get(context.Background())
+	}
+	return *authData, err
+}
+func AuthUpdateName(name string) error {
+	if authData == nil {
+		AuthGet()
+	}
+	authData.UserName = name
+	updatedAt, err := AuthRepo().UpdateName(context.Background(), name)
+	if err != nil {
+		return err
+	}
+	authData.UpdatedAt = updatedAt
+	return nil
+}
+func AuthUpdatePassWord(password string) error {
+	if authData == nil {
+		AuthGet()
+	}
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	authData.Password = string(hashedBytes)
+	updatedAt, err := AuthRepo().UpdatePassword(context.Background(), authData.Password)
+	if err != nil {
+		return err
+	}
+	authData.UpdatedAt = updatedAt
+	return nil
+}
+func AuthVerify(username, password string) error {
+	if authData == nil {
+		AuthGet()
+	}
+	if authData.UserName != username {
+		return fmt.Errorf("用户名不匹配")
+	}
+	return bcrypt.CompareHashAndPassword([]byte(authData.Password), []byte(password))
+}
