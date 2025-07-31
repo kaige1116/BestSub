f5ae83a1dbd553061d18d2ebbdce7c6fbe7f371c
bestruirui <bestrui@bestrui.top>
2025-07-31 15:15:44 +0800

    feat: :sparkles: 在debug模式下增强CORS中间件，允许所有跨域请求并添加OPTIONS请求处理

--- internal/server/middleware/cors.go
+++ internal/server/middleware/cors.go
@@ -7,10 +7,18 @@ import (
 )
 
 // CORS中间件
+// 在debug模式下允许所有跨域请求
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if utils.IsDebug() {
 			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With")
+			c.Header("Access-Control-Expose-Headers", "Content-Length")
+			if c.Request.Method == "OPTIONS" {
+				c.AbortWithStatus(204)
+				return
+			}
 		} else {
 			origin := c.Request.Header.Get("Origin")
 			if origin != "" {
@@ -18,13 +26,6 @@ func Cors() gin.HandlerFunc {
 					"BLOCKED", origin, c.Request.Host, c.Request.Method, c.Request.URL.Path,
 					c.ClientIP(), c.GetHeader("User-Agent"))
 			}
-		}
-		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Authorization")
-		c.Header("Access-Control-Expose-Headers", "Content-Length")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
 		}
 		c.Next()
 	}
