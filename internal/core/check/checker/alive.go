eca3a6fa2a9e23507eaf038af37a20d0fc7192ab
bestruirui <bestrui@bestrui.top>
2025-08-12 14:29:37 +0800

    refactor: :recycle: alive

--- internal/core/check/checker/alive.go
+++ internal/core/check/checker/alive.go
@@ -2,15 +2,15 @@ package checker
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
 	"sync"
 	"time"
 
-	"github.com/panjf2000/ants/v2"
+	"gopkg.in/yaml.v3"
 
 	"github.com/bestruirui/bestsub/internal/core/mihomo"
-	"github.com/bestruirui/bestsub/internal/core/nodepool"
+	"github.com/bestruirui/bestsub/internal/core/node"
+	"github.com/bestruirui/bestsub/internal/core/task"
 	checkModel "github.com/bestruirui/bestsub/internal/models/check"
 	nodeModel "github.com/bestruirui/bestsub/internal/models/node"
 	"github.com/bestruirui/bestsub/internal/modules/register"
@@ -34,35 +34,51 @@ func (e *Alive) Init() error {
 }
 
 func (e *Alive) Run(ctx context.Context, log *log.Logger, subID []uint16) checkModel.Result {
-	log.Infof("alive %s 任务执行开始 %d", e.URL, subID)
-	pool, _ := ants.NewPool(e.Thread)
-	defer pool.Release()
+	log.Infof("alive check task start, alive url: %s, thread: %d", e.URL, e.Thread)
+	var nodes []nodeModel.Data
+	if len(subID) == 0 {
+		nodes = node.GetAll()
+	} else {
+		nodes = *node.GetBySubId(subID)
+	}
+	threads := e.Thread
+	if threads <= 0 || threads > len(nodes) {
+		threads = len(nodes)
+	}
+	if threads > task.MaxThread() {
+		threads = task.MaxThread()
+	}
+	sem := make(chan struct{}, threads)
+
 	var wg sync.WaitGroup
-	for _, subID := range subID {
-		subStorage := nodepool.GetPoolBySubID(subID, 0)
-		for _, node := range subStorage.GetAllNode() {
-			wg.Add(1)
-			pool.Submit(func() {
-				var raw map[string]any
-				if err := json.Unmarshal(node.Raw, &raw); err != nil {
-					return
-				}
-				start := time.Now()
-				alive := e.detect(ctx, raw)
-				if alive {
-					log.Infof("节点 %s 存活 ✔", raw["name"].(string))
-				} else {
-					log.Infof("节点 %s 死亡 ✘", raw["name"].(string))
-				}
-				node.Info.SetAliveStatus(nodeModel.Alive, alive)
-				node.Info.Delay.Update(uint16(time.Since(start).Milliseconds()))
+	for _, nd := range nodes {
+		sem <- struct{}{}
+		wg.Add(1)
+		n := nd
+		task.Submit(func() {
+			defer func() {
+				<-sem
 				wg.Done()
-			})
-		}
+			}()
+			var raw map[string]any
+			if err := yaml.Unmarshal(n.Raw, &raw); err != nil {
+				log.Warnf("yaml.Unmarshal failed: %v", err)
+				return
+			}
+			log.Debugf("Start Name: %v", raw["name"])
+			start := time.Now()
+			alive := e.detect(ctx, raw)
+			if alive {
+				log.Debugf("Node %s is alive ✔", raw["name"].(string))
+			} else {
+				log.Debugf("Node %s is dead ✘", raw["name"].(string))
+			}
+			n.Info.SetAliveStatus(nodeModel.Alive, alive)
+			n.Info.Delay.Update(uint16(time.Since(start).Milliseconds()))
+		})
 	}
 	wg.Wait()
-	nodepool.UpdateStats()
-	log.Infof("alive %s 任务执行结束", e.URL)
+	log.Debugf("alive check task end, alive url: %s", e.URL)
 	return checkModel.Result{}
 }
 
@@ -82,10 +98,7 @@ func (e *Alive) detect(ctx context.Context, raw map[string]any) bool {
 		return false
 	}
 	defer response.Body.Close()
-	if response.StatusCode != e.ExptectCode {
-		return false
-	}
-	return true
+	return response.StatusCode == e.ExptectCode
 }
 
 func init() {
