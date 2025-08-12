186e2b099ff9a8f9b20a51837da23f72e17bc91e
bestruirui <bestrui@bestrui.top>
2025-08-12 17:23:41 +0800

    refactor: :recycle: 修改恢复中间件的日志级别，从错误日志改为警告日志

--- internal/server/middleware/recovery.go
+++ internal/server/middleware/recovery.go
@@ -8,10 +8,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Middleware 恢复中间件
 func Recovery() gin.HandlerFunc {
 	return gin.CustomRecovery(func(c *gin.Context, recovered interface{}) {
-		log.Errorf("Panic recovered: %v", recovered)
+		log.Warnf("Panic recovered: %v", recovered)
 		resp.Error(c, http.StatusInternalServerError, "An unexpected error occurred")
 		c.Abort()
 	})
