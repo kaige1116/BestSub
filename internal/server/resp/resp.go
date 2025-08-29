1826ce3b6d1ac42030994a18bd6ce169d7274c03
bestruirui <bestrui@bestrui.top>
2025-08-29 13:20:41 +0800

    refactor: :recycle: 统一响应结构体

--- internal/server/resp/resp.go
+++ internal/server/resp/resp.go
@@ -6,30 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// SuccessResponse 成功响应
-type SuccessStruct struct {
-	Code    int         `json:"code" example:"200"`        // 状态码
-	Message string      `json:"message" example:"success"` // 响应消息
-	Data    interface{} `json:"data,omitempty"`            // 响应数据
+type ResponseStruct struct {
+	Code    int         `json:"code" example:"200"`
+	Message string      `json:"message" example:"success"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
-// ErrorResponse 错误响应
-type ErrorStruct struct {
-	Code    int    `json:"code" example:"400"`                     // 状态码
-	Message string `json:"message" example:"error"`                // 响应消息
-	Error   string `json:"error" example:"Invalid request format"` // 错误详情
-}
-
-// PaginationResponse 分页响应结构
 type ResponsePaginationStruct struct {
-	Page     int         `json:"page" example:"1"`       // 当前页码
-	PageSize int         `json:"page_size" example:"10"` // 每页大小
-	Total    uint16      `json:"total" example:"100"`    // 总记录数
-	Data     interface{} `json:"data"`                   // 数据列表
+	Page     int         `json:"page" example:"1"`
+	PageSize int         `json:"page_size" example:"10"`
+	Total    uint16      `json:"total" example:"100"`
+	Data     interface{} `json:"data"`
 }
 
 func Success(c *gin.Context, data any) {
-	c.JSON(http.StatusOK, SuccessStruct{
+	c.JSON(http.StatusOK, ResponseStruct{
 		Code:    http.StatusOK,
 		Message: "success",
 		Data:    data,
@@ -37,18 +28,16 @@ func Success(c *gin.Context, data any) {
 }
 
 func Error(c *gin.Context, code int, err string) {
-	c.JSON(code, ErrorStruct{
+	c.JSON(code, ResponseStruct{
 		Code:    code,
-		Message: "error",
-		Error:   err,
+		Message: err,
 	})
 	c.Abort()
 }
 func ErrorBadRequest(c *gin.Context) {
-	c.JSON(http.StatusBadRequest, ErrorStruct{
+	c.JSON(http.StatusBadRequest, ResponseStruct{
 		Code:    http.StatusBadRequest,
-		Message: "error",
-		Error:   "bad request",
+		Message: "bad request",
 	})
 	c.Abort()
 }
