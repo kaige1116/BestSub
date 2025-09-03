d1149bd1755999cf2ad6d35af5c69acea55360a3
bestruirui <bestrui@bestrui.top>
2025-09-03 16:56:38 +0800

    feat: :sparkles: 添加随机UA

--- internal/modules/country/channel/commen.go
+++ internal/modules/country/channel/commen.go
@@ -1,11 +1,15 @@
 package channel
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/bestruirui/bestsub/internal/utils/ua"
+)
 
 type Common struct {
 	CountryCode string `json:"country_code"`
 }
 
 func UserAgent(req *http.Request) {
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.7204.168 Safari/537.36")
+	ua.SetHeader(req)
 }
